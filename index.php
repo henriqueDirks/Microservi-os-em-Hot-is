dad2f950c8bffb9597fa30ed3e1c1611f57e5a7d
henriqueDirks <henrique.d.bessa@academico.unirv.edu.br>
2024-05-12 16:12:12 -0300

    commit index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reserva de Hotel</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1>Bem-vindo à Reserva de Hotel</h1>
+
+    <div>
+        <a href="create_reserva.php" class="link">Criar Reserva</a>
+    </div>
+
+    <div>
+        <a href="delete_reserva.php" class="link">Deletar Reserva</a>
+    </div>
+</body>
+</html>
